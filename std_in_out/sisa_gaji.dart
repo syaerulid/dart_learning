f3d0877857d7b73c07376c7660a055a7a4cae758
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-11 07:07:41 +0700

    Create sisa_gaji.dart

---
+++ std_in_out/sisa_gaji.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+ 
+void main()
+{
+  print("Masukan jumlah Gajimu bulan ini:");
+  int? gaji = int.tryParse(stdin.readLineSync() ?? '') ?? 0; // use ? so we accepted null values
+  print('Kebutuhanmu bulan ini apa saja? (pisahkan dengan koma)');
+  String? kebutuhan = stdin.readLineSync();
+  
+  // Spli coma input dari list
+  List<String> kebutuhanList = kebutuhan?.split(',') ?? [];
+  
+  print('harga dari barang 1 sebesar:');
+  int? item1 = int.tryParse(stdin.readLineSync() ?? '') ?? 0; // sabun
+  print('harga dari barang 2 sebesar:');
+  int? item2 = int.tryParse(stdin.readLineSync() ?? '') ?? 0; // sampo
+  print('harga dari barang 3 sebesar:');
+  int? item3 = int.tryParse(stdin.readLineSync() ?? '') ?? 0; // seblak
+  
+  // kondisi gaji
+  if (gaji != null) 
+   // printing the gaji if it's not null
+  {
+    print("Gajimu bulan ini adalah sebesar: $gaji");
+    print("dan kebutuhanmu bulan ini adalah: $kebutuhan");
+    
+    // harga tiap barang
+    print('Harga dari sabun sebesar: $item1');
+    print('Harga dari sampo sebesar: $item2');
+    print('Harga dari seblak sebesar: $item3');
+    
+    // sisa gaji
+    var sisa_gaji = gaji - (item1 + item2 + item3);
+    
+    // sisa gaji
+    print('Sisa gajimu sekarang sebesar: $sisa_gaji');
+  } 
+  else 
+  {
+     print('Input tidak valid. Mohon masukan gaji dalam bentuk angka.');
+  };
+}
+
+
