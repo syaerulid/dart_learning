901a3f0faacd9eb8e52b29db40400543fd7cdaf2
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 21:35:22 +0700

    Create abstract.dart

---
+++ dart_fundamental/abstract.dart
@@ -0,0 +1,11 @@
+Untuk menjadikan sebuah kelas menjadi abstract, kita hanya perlu menambah keyword abstract sebelum penulisan kelas:
+
+abstract class Animal {
+  String name;
+  int age;
+  double weight;
+ 
+  // ...
+}
+
+// Dengan begitu kelas Animal tidak dapat diinisialisasikan menjadi sebuah objek.
