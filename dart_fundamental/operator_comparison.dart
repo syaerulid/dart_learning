44266ceff6515b1ae91fa04f085009a8862fd702
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 09:21:23 +0700

    Create operator_comparison.dart

---
+++ dart_fundamental/operator_comparison.dart
@@ -0,0 +1,29 @@
+// Operator comparison
+void main() {
+    // ==
+    var a = 5;
+    var b = 7;
+    
+    if (a == b)
+    print('True'); 
+    else
+    print('False');
+    
+    // !=
+    if (a != b)
+    print('True');
+    else
+    print('False');
+    
+    // >
+    if (a > b)
+    print('True');
+    else
+    print('False');
+    
+    // <
+    if (a < b)
+    print('True');
+    else
+    print('False');
+}
