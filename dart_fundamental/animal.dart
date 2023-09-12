085816b336f5b247993877171770585f7e1c95a2
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 20:56:14 +0700

    Create animal.dart

---
+++ dart_fundamental/animal.dart
@@ -0,0 +1,17 @@
+// make class to a library that we can import
+class Animal {
+  String _name = '';
+  int _age = 0;
+  double _weight = 0;
+}
+
+import 'animal.dart';
+
+void main() {
+  var dicodingCat = Animal('Gray', 2, 4.2);
+
+  dicodingCat.eat();
+  dicodingCat.poop();
+
+  print(dicodingCat.weight);
+}
