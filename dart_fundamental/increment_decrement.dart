bce46723a282ccdae7bfbb2d5737506f395e8e0b
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 09:16:09 +0700

    Create increment_decrement.dart

---
+++ dart_fundamental/increment_decrement.dart
@@ -0,0 +1,23 @@
+// Increment and decrement in dart
+
+void main() {
+    var a = 0;
+    var b = 5;
+    
+    a++; // increment in python its same like a += 1
+    b--; // decrement, in python its same like a -= 1
+    
+    print(a);
+    print(b);
+    
+    // Increment and while loops
+    var c = 3;
+    while (c < 7) {
+        c++; 
+        if (c == 7) {
+            break;
+        }
+    print(c);
+    }
+    
+}
