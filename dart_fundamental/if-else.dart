c4d56c989a3e7fa0e29f0cc3d08d22fc46d3df96
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 10:59:52 +0700

    Create if-else.dart

---
+++ dart_fundamental/if-else.dart
@@ -0,0 +1,21 @@
+void main() {
+  // if
+  var isRaining = true;
+
+  print('Prepare before going to office.');
+  if (isRaining) {
+    print("Oh. It's raining, bring an umbrella.");
+  }
+  print('Going to the office.');
+
+  // if else
+  var openHours = 8;
+  var closedHours = 21;
+  var now = 17;
+
+  if (now > openHours && now < closedHours) {
+    print("Hello, we're open");
+  } else {
+    print("Sorry, we've closed");
+  }
+}
