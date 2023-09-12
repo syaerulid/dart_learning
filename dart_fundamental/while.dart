748556e0440e1d5a1ff008a737b9215beb3906a3
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 11:16:16 +0700

    Create while.dart

---
+++ dart_fundamental/while.dart
@@ -0,0 +1,19 @@
+// while
+void main() {
+  var i = 1;
+
+  while (i <= 5) {
+    print(i);
+    i++;
+  }
+}
+
+// do-while
+void main() {
+  var i = 1;
+
+  do {
+    print(i);
+    i++;
+  } while (i <= 7);
+}
