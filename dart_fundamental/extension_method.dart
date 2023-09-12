040f7379dab36a9e9bdf0fb6ec453fc2b0824ff0
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-12 23:58:46 +0700

    Create extension_method.dart

---
+++ dart_fundamental/extension_method.dart
@@ -0,0 +1,34 @@
+// Extension Method
+// Cara membuat fungsionalitas tambahan dari library yang sudah ada
+
+void main() {
+  var unsortedNumbers = [2, 5, 3, 1, 4];
+  print(unsortedNumbers);
+
+  var sortedNumbers = unsortedNumbers.sortAsc();
+
+  print(sortedNumbers);
+}
+
+// simpan di extensions.dart
+extension Sorting on List<int> {
+  List<int> sortAsc() {
+    var list = this;
+    var length = this.length;
+
+    for (int i = 0; i < length - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < length; j++) {
+        if (list[j] < list[min]) {
+          min = j;
+        }
+      }
+
+      int tmp = list[min];
+      list[min] = list[i];
+      list[i] = tmp;
+    }
+
+    return list;
+  }
+}
