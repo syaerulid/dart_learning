4caae3f203cc9052566c0cbc7c5223cb162c0b08
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-13 09:10:30 +0700

    Create higher-order_func.dart

---
+++ dart_fundamental/functionalStyles/higher-order_func.dart
@@ -0,0 +1,23 @@
+// function that use other function as parameter
+void myHigherOrderFunction(String message, Function myFunction) {
+  print(message);
+  print(myFunction(3, 4));
+}
+
+// Example
+void main() {
+  // Opsi 1
+  int Function(int num1, int num2) sum = (int num1, int num2) => num1 + num2;
+  myHigherOrderFunction('Hello', sum);
+
+  // Opsi 2
+  myHigherOrderFunction('Hello', (num1, num2) => num1 + num2);
+}
+
+void myHigherOrderFunction(
+  String message,
+  int Function(int num1, int num2) myFunction,
+) {
+  print(message);
+  print(myFunction(3, 4));
+}
