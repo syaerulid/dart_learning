5366e116538a686f41bb0b6427e5359bb0bc3003
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-13 10:00:49 +0700

    Create syncAsynch.dart

---
+++ dart_fundamental/dartFutures/syncAsynch.dart
@@ -0,0 +1,7 @@
+Synchronous vs Asynchronous
+Dalam synchronous program, kode program dijalankan secara berurutan dari atas ke bawah. Artinya jika kita menuliskan dua baris kode maka baris kode kedua tidak bisa dieksekusi sebelum kode baris pertama selesai dieksekusi. 
+Kita bisa bayangkan ketika sedang berada dalam antrian kasir. 
+Kita tidak akan dilayani sebelum semua antrian di depan kita selesai dilayani, begitu pula orang di belakang kita pun harus menunggu gilirannya.
+
+Dalam asynchronous program, jika kita menuliskan dua baris kode, kita dapat membuat baris kode kedua dieksekusi tanpa harus menunggu kode pada baris pertama selesai dieksekusi. 
+Dalam dunia nyata bisa kita bayangkan dengan memesan kopi melalui pelayan, di mana sembari menunggu pesanan kita datang, kita dapat melakukan aktivitas lain seperti membuka laptop atau menulis.
