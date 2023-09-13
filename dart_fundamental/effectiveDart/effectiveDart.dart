cfe100cb887acce8f8a00a7223a7657140274be3
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-13 10:26:02 +0700

    Create effectiveDart.dart

---
+++ dart_fundamental/effectiveDart/effectiveDart.dart
@@ -0,0 +1,7 @@
+Effective Dart memiliki banyak rules. Masing-masing aturan akan diawali dengan kata kunci untuk menentukan bagaimana sifat dari aturan tersebut. Lima kata kunci tersebut, antara lain:
+
+DO. Ketika aturan diawali dengan DO maka praktik tersebut harus selalu diikuti.
+DON’T. Sebaliknya, aturan yang diawali dengan DON’T bukan merupakan hal yang baik untuk diterapkan.
+PREFER. Ini adalah praktik yang harus diikuti. Namun, mungkin ada keadaan di mana lebih masuk akal untuk melakukan sebaliknya. Pastikan Anda memahami konsekuensi ketika Anda mengabaikan aturan ini.
+AVOID. Ini adalah kebalikan dari PREFER. Panduan ini menjelaskan hal-hal yang tidak boleh dilakukan, namun kemungkinan ada alasan bagus untuk melakukannya pada beberapa kejadian.
+CONSIDER. Panduan ini adalah praktik yang bisa Anda ikuti atau tidak Anda ikuti, tergantung pada keadaan dan preferensi Anda sendiri.
