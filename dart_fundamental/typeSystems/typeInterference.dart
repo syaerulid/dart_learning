079126534e5387d526ffc52883f490836fde3974
Erul - Bukan Irul <119069839+syaerulid@users.noreply.github.com>
2023-09-13 09:55:12 +0700

    Create typeInterference.dart

---
+++ dart_fundamental/typeSystems/typeInterference.dart
@@ -0,0 +1,5 @@
+// 
+Map<String, dynamic> company = {'name': 'Dicoding', 'yearsFounded': 2015};
+
+// gunakan var dan dart akan menentukan tipenya
+var company = {'name': 'Dicoding', 'yearsFounded': 2015}; 
